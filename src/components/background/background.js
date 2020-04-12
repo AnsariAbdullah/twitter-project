ddb0fe3562ab98145997ebee9d631ae4ead5e2fa
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-04-12 19:36:51 +0530

    imitate blog file

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './background.scss'
 
-const Background = () => {
-	return (
-		<div className="title">
-			Background
-			<h1 className="sub-title">Saas test</h1>
-			<h2>
-				mini title
-			</h2>
-		</div>
-	);
+class Background extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			pictures: [],
+		}
+	}
+	componentDidMount() {
+		fetch('https://randomuser.me/api/?results=50')
+		.then(results => {
+			return results.json();
+		}).then(data => {
+			let pictures = data.results.map((pic) => {
+				return(
+					<div key={pic.results}>
+						<img src={pic.pictures.medium} />
+					</div>
+				)
+			})
+			this.setState({pictures: pictures});
+			console.log("state", this.state.pictures)
+		})
+	}
+	render() { 
+		return (
+			<div className="container">
+				{this.state.pictures}
+			</div>
+		);
+	}
 }
  
 export default Background;
