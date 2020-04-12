21fdb1b4c909467edc208a2c4017d605d740d10b
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-04-12 14:14:36 +0530

    test sass

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './background.scss'
 
 const Background = () => {
 	return (
-		<div>
+		<div className="title">
 			Background
+			<h1 className="sub-title">Saas test</h1>
+			<h2>
+				mini title
+			</h2>
 		</div>
 	);
 }
